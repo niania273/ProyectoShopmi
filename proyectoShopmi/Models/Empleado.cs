5af5188c00c246a6d5e5524931a1c63d33b39d6c
Fabiana Canales <f.luciacanales@gmail.com>
2025-04-05 18:07:48 -0500

    Primer commit

---
+++ proyectoShopmi/Models/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectoShopmi.Models
+{
+    public class Empleado
+    {
+        [Key]
+        [DisplayName("Código")]
+        public int codEmp { get; set; }
+        
+        [DisplayName("DNI")]
+        public string? numDoc { get; set; }
+        
+        [DisplayName("Nombres")]
+        public string? nomEmp { get; set; }
+        
+        [DisplayName("Apellidos")]
+        public string? apeEmp { get; set; }
+        
+        [DisplayName("Dirección")]
+        public string? dirEmp { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Fecha de Nacimiento")]
+        public DateTime fechaNac { get; set; }
+
+        [DisplayName("Sexo")]
+        public string? sexEmp { get; set; }
+
+        [DisplayName("Correo")]
+        public string? corEmp { get; set; }
+
+        [DisplayName("Estado")]
+        public bool estEmp { get; set; }
+
+        //Relación Empleado y Distrito
+        [DisplayName("Distrito")]
+        public int codDis { get; set; }
+        public Distrito? Distrito { get; set; }
+    }
+}
---
+++ proyectoShopmi/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectoShopmi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DisplayName("Código")]
+        public int codUsu { get; set; }
+
+        [DisplayName("Nombres")]
+        public string? nomUsu { get; set; }
+        
+        [DisplayName("Apellidos")]
+        public string? apeUsu { get; set; }
+
+        [DisplayName("Correo")]
+        public string? corUsu { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Fecha de Creación")]
+        public string? fecCre { get; set; }
+
+        [DisplayName("Estado")]
+        public string? estUsu { get; set; }
+
+        //Relación Usuario y Empleado
+        [DisplayName("Referencia Empleado")]
+        public int codEmp { get; set; }
+        public Empleado? Empleado { get; set; }
+
+        //Relación Usuario y Rol
+        [DisplayName("Referencia Rol")]
+        public int codRol { get; set; }
+        public Rol? Rol { get; set; }
+
+    }
+}
