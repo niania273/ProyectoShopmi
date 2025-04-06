4dba8559c4bef65c686ce22bfd520ec953a4a306
ashlyy09 <ashlyatt9@gmail.com>
2025-04-06 17:34:56 -0500

    Update Categoria.cs

--- proyectoShopmi/Models/Categoria.cs
+++ proyectoShopmi/Models/Categoria.cs
@@ -1,1 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectoShopmi.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int codcategoria { get; set; }
+        public string nombremarca { get; set; }
+        public bool estmarca { get; set; }
+    }
+}
 
