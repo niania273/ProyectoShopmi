f6eb9f0aab912fdf7c4ff65c143d0c218f752ee4
ashlyy09 <ashlyatt9@gmail.com>
2025-04-07 18:04:07 -0500

    Update Categoria.cs

--- proyectoShopmi/Models/Categoria.cs
+++ proyectoShopmi/Models/Categoria.cs
@@ -6,9 +6,9 @@ namespace proyectoShopmi.Models
     {
         [Key]
         public int codcategoria { get; set; }
-        [DisplayName("Nombre de la Marca")]
-        public string nombremarca { get; set; }
-        public bool estmarca { get; set; }
+        [DisplayName("Nombre de la Categoria")]
+        public string nomcategoria { get; set; }
+        public bool estcategoria { get; set; }
     }
 }
 
