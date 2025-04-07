479b7bceff325ebff24504037b79d70036907c45
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-07 17:23:21 -0500

    Update Pedido.cs

--- proyectoShopmi/Models/Pedido.cs
+++ proyectoShopmi/Models/Pedido.cs
@@ -1,1 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
+namespace proyectoShopmi.Models
+{
+    public class Pedido
+    {
+        [Key]
+        [DisplayName("Código")]
+        public int codpedido { get; set; }
+        
+        [DisplayName("Cliente")]
+        public int codcliente { get; set; }
+      
+        [DisplayName("Empleado")]
+        public int codempleado { get; set; }  
+        
+        [DisplayName("Fecha del Pedido")]
+        public DateTime fecped { get; set; }
+        
+        [DisplayName("Precio Total")]
+        public decimal preciototal { get; set; }
+
+        [DisplayName("Cantidad")]
+        public int cantidad { get; set; }
+
+        [DisplayName("Estado")]
+        public bool estped { get; set; }
+
+        //Relación Empleado y Distrito
+        public int codDis { get; set; }
+        public Distrito? Distrito { get; set; }
+    }
+}
