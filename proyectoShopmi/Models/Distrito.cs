770143884f99aa6801265d488f52b3fade70cbee
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-07 17:11:05 -0500

    Update Distrito.cs

--- proyectoShopmi/Models/Distrito.cs
+++ proyectoShopmi/Models/Distrito.cs
@@ -2,5 +2,14 @@
 {
     public class Distrito
     {
+        [Key]
+        public int coddsitrito { get; set; }
+        
+        [DisplayName("Nombre del distrito")]
+        public string nombredistrito { get; set; }
+        
+        public string codpostal { get; set; }
+        
+        public bool estdistrito { get; set; }
     }
 }
