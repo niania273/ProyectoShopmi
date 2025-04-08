30379cf93702e352cd46955da5f0d2f701d14309
ashlyy09 <ashlyatt9@gmail.com>
2025-04-07 19:43:57 -0500

    Update ICategoria.cs

--- proyectoShopmi/Repositorio/Interfaces/ICategoria.cs
+++ proyectoShopmi/Repositorio/Interfaces/ICategoria.cs
@@ -1,1 +1,13 @@
+using proyectoShopmi.Models;
+
+namespace proyectoShopmi.Repositorio.Interfaces
+{
+    public interface ICategoria
+    {
+        Task<Categoria> getCategoria(int codcategoria);
+        Task<string> insertCategoria(Categoria categoria);
+        Task<string> updateCategoria(Categoria categoria);
+
+    }
+}
 
