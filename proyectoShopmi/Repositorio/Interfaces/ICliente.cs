869b4703d6b1dffbf552409e615457d7d3ecf784
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-07 19:23:25 -0500

    Update ICliente.cs

--- proyectoShopmi/Repositorio/Interfaces/ICliente.cs
+++ proyectoShopmi/Repositorio/Interfaces/ICliente.cs
@@ -4,11 +4,8 @@ namespace proyectoShopmi.Repositorio.Interfaces
 {
     public interface ICliente
     {
-        IEnumerable<Cliente> getClientes();
-
         Task<Cliente> getCliente(int codCliente);
         Task<string> insertCliente(Cliente cliente);
         Task<string> updateCliente(Cliente cliente);
-
     }
 }
