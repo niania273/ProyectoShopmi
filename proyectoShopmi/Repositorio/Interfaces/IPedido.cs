a6d766a04355c28f7ed921f7c4a3cb3b9c0800f2
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-07 19:25:42 -0500

    Update IPedido.cs

--- proyectoShopmi/Repositorio/Interfaces/IPedido.cs
+++ proyectoShopmi/Repositorio/Interfaces/IPedido.cs
@@ -4,8 +4,6 @@ namespace proyectoShopmi.Repositorio.Interfaces
 {
     public interface IPedido
     {
-        IEnumerable<Pedido> getPedidos();
-
         Task<Pedido> getPedido(int codPedido);
         Task<string> insertPedido(Pedido pedido);
         Task<string> updatePedido(Pedido pedido);
