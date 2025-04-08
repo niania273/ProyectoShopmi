de4265a7e2acab5f293683dab7257475d7b1b56d
Yari09210 <Yari.ha.09210@gmail.com>
2025-04-07 19:08:03 -0500

    Create IMarca.cs

---
+++ proyectoShopmi/Repositorio/Interfaces/IMarca.cs
@@ -0,0 +1,13 @@
+using proyectoShopmi.Models;
+
+namespace proyectoShopmi.Repositorio.Interfaces
+{
+    public interface IMarca
+    {
+        public Task<IEnumerable<Marca>> GetMarcas()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
