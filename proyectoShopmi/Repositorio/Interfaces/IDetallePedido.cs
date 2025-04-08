ce25a09aadadae0ee761cbd022c58ba1aa698e46
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-07 19:28:50 -0500

    Update IDetallePedido.cs

--- proyectoShopmi/Repositorio/Interfaces/IDetallePedido.cs
+++ proyectoShopmi/Repositorio/Interfaces/IDetallePedido.cs
@@ -4,8 +4,6 @@ namespace proyectoShopmi.Repositorio.Interfaces
 {
     public interface IDetallePedido
     {
-        IEnumerable<DetallePedido> getDetallePedidos();
-
         Task<DetallePedido> getDetallePedido(int codDetallePedido);
         Task<string> insertDetallePedido(DetallePedido detallePedido);
         Task<string> updateDetallePedido(DetallePedido detallePedido);
