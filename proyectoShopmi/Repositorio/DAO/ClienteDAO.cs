0ff2b0facadaaca44c75aa63d910cbb22e2a6baf
ashlyy09 <ashlyatt9@gmail.com>
2025-04-12 18:36:10 -0500

    Update ClienteDAO.cs

--- proyectoShopmi/Repositorio/DAO/ClienteDAO.cs
+++ proyectoShopmi/Repositorio/DAO/ClienteDAO.cs
@@ -1,7 +1,6 @@
 using Microsoft.Data.SqlClient;
 using proyectoShopmi.Models;
 using proyectoShopmi.Repositorio.Interfaces;
-using System.Data;
 using Dapper;
 
 namespace proyectoShopmi.Repositorio.DAO
@@ -9,14 +8,30 @@ namespace proyectoShopmi.Repositorio.DAO
 
     public class ClienteDAO : ICliente
     {
-        private readonly string cadena;
+        private readonly string cadena ="";
 
         public ClienteDAO()
         {
             cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("Conexion") ?? "";
 
         }
-        public async Task<Cliente> getCliente(int codCliente)
+
+        public async Task<IEnumerable<Cliente>> GetClientes()
+        {
+            var sp = "USP_GET_CLIENTE";
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var listado = await conexion.QueryAsync<Cliente>(sp);
+                return listado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Cliente> GetCliente(int codCliente)
         {
             var sp = "USP_GET_ID_CLIENTE";
             var parameters = new DynamicParameters();
@@ -26,7 +41,7 @@ namespace proyectoShopmi.Repositorio.DAO
             {
                 using var conexion = new SqlConnection(cadena);
 
-                var registro = await conexion.QueryFirstOrDefaultAsync<Cliente>(sp, parameters, commandType: CommandType.StoredProcedure);
+                var registro = await conexion.QueryFirstOrDefaultAsync<Cliente>(sp);
 
                 return registro;
             }
@@ -35,16 +50,58 @@ namespace proyectoShopmi.Repositorio.DAO
                 throw new Exception(ex.Message);
             }
         }
-        public Task<string> insertCliente(Cliente cliente)
-        {
-            throw new NotImplementedException();
-        }
+        
 
-        public Task<string> updateCliente(Cliente cliente)
+        public async Task<string> MergeCliente(Cliente cliente)
         {
-            throw new NotImplementedException();
-        }
+            var sp = "USP_GET_MERGE_CLIENTE";
+            var mensaje = "";
+            var parameters = new DynamicParameters();
 
+            parameters.Add("CODCLIENTE", cliente.codcliente, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            parameters.Add("NUMEROCOMDOCUMENTO", cliente.numerocomdocumento, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("APELLIDOS", cliente.apellidos, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("NOMBRES", cliente.nombres, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("CODDISTRITO", cliente.coddistrito, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            parameters.Add("DIRECCION", cliente.direccion, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("FECNAC", cliente.fecnac, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("SEXO", cliente.sexo, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("CORREO", cliente.correo, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("TELEFONO", cliente.telefono, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameters.Add("ESTCLIENTE", cliente.estcliente, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var respuesta = await conexion.ExecuteAsync(sp, parameters);
+                mensaje = $"Se ha realizado la {respuesta} de {respuesta} cliente.";
+                return mensaje;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<string> DeleteCliente(int codcliente)
+        {
+            var sp = "USP_DELETE_CLIENTE";
+            var mensaje = "";
+            var parameters = new DynamicParameters();
+
+            parameters.Add("CODCLIENTE", codcliente, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var respuesta = await conexion.ExecuteAsync(sp, parameters);
+                mensaje = $"Se ha eliminado {respuesta} cliente.";
+                return mensaje;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
     }
 }
