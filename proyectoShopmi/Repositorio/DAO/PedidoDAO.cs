ebd17314630b6936859e93c9cb0fd693fc2fef0e
Miguel012601 <miguuelarteagaj@gmail.com>
2025-04-13 03:14:48 -0500

    Update PedidoDAO.cs

--- proyectoShopmi/Repositorio/DAO/PedidoDAO.cs
+++ proyectoShopmi/Repositorio/DAO/PedidoDAO.cs
@@ -13,9 +13,24 @@ namespace proyectoShopmi.Repositorio.DAO
         public PedidoDAO()
         {
             cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("conexion") ?? "";
-
         }
-        public async Task<Pedido> getPedido(int codPedido)
+
+        public async Task<IEnumerable<Pedido>> GetPedidos()
+        {
+            var sp = "USP_GET_PEDIDO";
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var listado = await conexion.QueryAsync<Pedido>(sp, commandType: CommandType.StoredProcedure);
+                return listado;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Pedido> GetPedido(int codPedido)
         {
             var sp = "USP_GET_ID_PEDIDO";
             var parameters = new DynamicParameters();
@@ -34,14 +49,48 @@ namespace proyectoShopmi.Repositorio.DAO
                 throw new Exception(ex.Message);
             }
         }
-        public Task<string> insertPedido(Pedido pedido)
+        public async Task<string> MergePedido(Pedido pedido, string accion)
         {
-            throw new NotImplementedException();
+            var sp = "USP_MERGE_PEDIDO";
+            var parameters = new DynamicParameters();
+
+            parameters.Add("CODPEDIDO", pedido.codPedido, DbType.Int32, ParameterDirection.Input);
+            parameters.Add("CODCLIENTE", pedido.codCliente, DbType.Int32, ParameterDirection.Input);
+            parameters.Add("CODEMPLEADO", pedido.codEmpleado, DbType.Int32, ParameterDirection.Input);
+            parameters.Add("FECPED", pedido.fecPed, DbType.Date, ParameterDirection.Input);
+            parameters.Add("PRECIOTOTAL", pedido.precioTotal, DbType.Decimal, ParameterDirection.Input);
+            parameters.Add("ACTPED", pedido.actPed, DbType.String, ParameterDirection.Input);
+            parameters.Add("ESTPED", pedido.estPed, DbType.Boolean, ParameterDirection.Input);
+
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var resultado = await conexion.ExecuteAsync(sp, parameters, commandType: CommandType.StoredProcedure);
+                return $"Se ha realizado la {accion} de {(resultado > 0 ? "1" : "ningún")} pedido.";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al ejecutar MergePedido: " + ex.Message);
+            }
         }
 
-        public Task<string> updatePedido(Pedido pedido)
+        public async Task<string> DeletePedido(int codPedido)
         {
-            throw new NotImplementedException();
+            var sp = "USP_DELETE_PEDIDO";
+            var parameters = new DynamicParameters();
+
+            parameters.Add("CODPEDIDO", codPedido, DbType.Int32, ParameterDirection.Input);
+
+            try
+            {
+                using var conexion = new SqlConnection(cadena);
+                var respuesta = await conexion.ExecuteAsync(sp, parameters, commandType: CommandType.StoredProcedure);
+                return $"Se ha realizado la eliminación de {(respuesta > 0 ? "1" : "ningún")} pedido.";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al eliminar el pedido: " + ex.Message);
+            }
         }
 
     }
