9fa374825580bce348be08f1c59fc1e47f094b9d
Yari09210 <Yari.ha.09210@gmail.com>
2025-04-13 02:18:45 -0500

    Add files via upload

---
+++ proyectoShopmi/Repositorio/Interfaces/IProducto.cs
@@ -0,0 +1,15 @@
+﻿using proyectoShopmi.Models;
+
+namespace proyectoShopmi.Repositorio.Interfaces
+{
+    public interface IProducto
+    {
+        Task<IEnumerable<Producto>> GetProductos();
+
+        Task<Producto> GetProducto(int codProducto);
+
+        Task<string> MergeProducto(Producto producto);
+
+        Task<string> DeleteProducto(int codProducto);
+    }
+}
