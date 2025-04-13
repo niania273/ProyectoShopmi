36333c39f992e5e83af94d1ddd185b10b15a86f2
Yari09210 <Yari.ha.09210@gmail.com>
2025-04-13 04:02:39 -0500

    Update MarcaDAO.cs

--- proyectoShopmi/Repositorio/DAO/MarcaDAO.cs
+++ proyectoShopmi/Repositorio/DAO/MarcaDAO.cs
@@ -22,7 +22,7 @@ namespace proyectoShopmi.Repositorio.DAO
             try
             {
                 using var conexion = new SqlConnection(cadena);
-                var listado = await conexion.QueryAsync<Marca>(sp);
+                var listado = await conexion.QueryAsync<Marca>(sp, commandType: CommandType.StoredProcedure);
                 return listado;
             }
             catch (Exception ex)
