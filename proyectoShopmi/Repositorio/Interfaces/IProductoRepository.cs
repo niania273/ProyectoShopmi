adb6f491b249f66bc29973b1b7ebc534a52b8847
Sabrina <sabrinareyna777@gmail.com>
2025-04-21 21:10:48 -0500

    Update IProductoRepository.cs

--- proyectoShopmi/Repositorio/Interfaces/IProductoRepository.cs
+++ proyectoShopmi/Repositorio/Interfaces/IProductoRepository.cs
@@ -6,6 +6,7 @@ namespace proyectoShopmi.Repositorio.Interfaces
     public interface IProductoRepository
     {
         Task<IEnumerable<ProductoResponse>> GetProductos();
+        Task<IEnumerable<ProductoResponse>> GetProductosPorCategoria(int codCategoria);
         Task<ProductoResponse> GetProducto(int codProducto);
         Task<string> MergeProducto(ProductoRequest producto, string accion);
         Task<string> DeleteProducto(int codProducto);
